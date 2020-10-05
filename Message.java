d42f57dcd861eb8053da9f0a9573190222e39d3f
Stefan John Bosco <55599785+TheCriminelz@users.noreply.github.com>
2020-10-05 13:13:24 -0400

    update

--- Message.java
+++ Message.java
@@ -18,42 +18,42 @@ public class Message {
         message = "";
         date = new Date();
     }
-    public Message(String username, String message, Date date)
-    {
+    public Message(String username, String message, Date date) {
         this.username = username;
         this.message = message;
         this.date = date;
     }
 
-    public String getUsername()
-    {
+    public Message(String message, Date date) {
+        this("Anonymous", message, date);
+    }
+
+    public String getUsername() {
         return username;
     }
-    public String getMessage()
-    {
+
+    public String getMessage() {
         return message;
     }
-    public Date getDate()
-    {
+
+    public Date getDate() {
         return date;
     }
 
-    public void setUsername(String username)
-    {
+    public void setUser(String username) {
         this.username = username;
     }
-    public void setMessage(String message)
-    {
+
+    public void setContent(String message) {
         this.message = message;
     }
-    public void setDate(Date date)
-    {
+
+    public void setDate(Date date) {
         this.date = date;
     }
 
-
     @Override
     public String toString() {
-        return "USERNAME: " + getUsername() + "\nMESSAGE: " + getMessage()+ "\nDATE: " + getDate() +"\n";
+        return this.username + ": " + this.message;
     }
 }
--- PostMessageSevlet.java
+++ PostMessageSevlet.java
@@ -1,19 +1,17 @@
-import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 
-@WebServlet(name = "post_message")
-public class post_message extends HttpServlet {
+@WebServlet(name = "PostMessageSevlet")
+public class PostMessageSevlet extends HttpServlet {
     ArrayList<Message> msg = new ArrayList<Message>();
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String message = request.getParameter("message");
         request.getSession().setAttribute("username", username);
@@ -24,8 +22,11 @@ public class post_message extends HttpServlet {
 
         System.out.println(msg.toString());
 
-        RequestDispatcher req = request.getRequestDispatcher("message.jsp");
+        RequestDispatcher req = request.getRequestDispatcher("index.jsp");
         req.forward(request, response);
     }
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
