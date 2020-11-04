833e1622803053872675612505aae9d8e1d7894f
Stefan John Bosco <55599785+TheCriminelz@users.noreply.github.com>
2020-11-04 16:03:37 -0500

    added UserManager

--- src/main/java/AuthServlet.java
+++ src/main/java/AuthServlet.java
@@ -19,56 +19,20 @@ public class AuthServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String uname = request.getParameter("username");
         String password = request.getParameter("password");
-        String generatedPassword = null;
-        try {
-            // Create MessageDigest instance for MD5
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            //Add password bytes to digest
-            md.update(password.getBytes());
-            //Get the hash's bytes
-            byte[] bytes = md.digest();
-            //This bytes[] has bytes in decimal format;
-            //Convert it to hexadecimal format
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < bytes.length; i++) {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-            //Get complete hashed password in hex format
-            generatedPassword = sb.toString();
-        }
-        catch (NoSuchAlgorithmException e)
-        {
-            e.printStackTrace();
-        }
 
-        UserFileDaoImpl userDao = new UserFileDaoImpl();
         HttpSession session = request.getSession();
         String destPage = "login.jsp";
+        UserManager manager = new UserManager();
 
         try {
-            User user = userDao.getUserByUsername(uname);
-
-            String pass = user.getPassword();
-            String username = user.getUsername();
-            int userID = user.getUserID();
-
-            if (user != null && generatedPassword.equals(pass)) {
-                String errMsg = (String)session.getAttribute("errorMessage");
-                if(errMsg != null)
+                if(manager.login(uname, password, session))
                 {
-                    session.removeAttribute("errorMessage");
+                    destPage = "index.jsp";
                 }
-
-                session.setAttribute("userID", userID);
-                session.setAttribute("username", username);
-                destPage = "index.jsp";
-            } else {
-                String message = "Invalid username/password";
-                System.out.println("Invalid Username or Password");
-                session.setAttribute("errorMessage", message);
-                request.setAttribute("message", message);
-            }
-
+                else
+                {
+                    destPage = "login.jsp";
+                }
             response.sendRedirect(destPage);
             //RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
             //dispatcher.forward(request, response);
