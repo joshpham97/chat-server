7b6574bab04cb5bb05675d0dca67a5b15d7a1362
Timothy <exzos_graalonline@hotmail.com>
2020-12-06 22:53:18 -0500

    usermanager unit tests

--- src/main/java/app/UserManager.java
+++ src/main/java/app/UserManager.java
@@ -4,4 +4,6 @@ import javax.servlet.http.HttpSession;
 
 public interface UserManager {
     public boolean login(String username, String password, HttpSession session);
+
+    public String encryptPassword(String password);
 }
